66eeb8c38472af2eb9158fbffe14ff95de37044b
Elen Vieira <elenv.vieiracorporativo@gmail.com>
2026-03-18 22:13:09 -0300

    Atualizações

---
+++ sql-basics/join-vio.sql
@@ -0,0 +1,29 @@
+-- join para listar todos os ingressos, incluindo os que não foram vendidos
+select
+    i.id_ingresso, 
+    i.tipo, 
+    ic.quantidade,
+    round ((i.preco * ic.quantidade), 2) as valor_total
+from
+    ingresso i left join ingresso_compra ic on i.id_ingresso =  ic.fk_id_ingresso 
+order by id_ingresso;    
+
+-- visualizando apenas os ingressos que não foram comprados utilizando where
+select
+    i.id_ingresso, 
+    i.tipo, 
+    ic.quantidade,
+    round ((i.preco * ic.quantidade), 2) as valor_total
+from
+    ingresso i left join ingresso_compra ic on i.id_ingresso =  ic.fk_id_ingresso 
+where quantidade is null;
+
+-- visualizando os ingressos que não foram comprados pela existencia da fk_id_compra (só existe se existir compra)
+select
+    i.id_ingresso, 
+    i.tipo, 
+    ic.quantidade,
+    round ((i.preco * ic.quantidade), 2) as valor_total
+from
+    ingresso i left join ingresso_compra ic on i.id_ingresso =  ic.fk_id_ingresso 
+where fk_id_compra is null;
